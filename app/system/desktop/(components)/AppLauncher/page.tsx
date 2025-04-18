b283c7df2693b8c034547b9c80670623b69cf9e0
BorDevTech <bordevtech@icloud.com>
2025-04-18 19:14:54 -0400

    refactor: restructure AppLauncher component layout and improve readability

--- app/system/desktop/(components)/AppLauncher/page.tsx
+++ app/system/desktop/(components)/AppLauncher/page.tsx
@@ -62,87 +62,89 @@ export default function AppLauncher({
   //   </GridItem>;
 
   return (
-    <GridItem rowStart={1} rowSpan={screenSize.y - 1} colSpan={screenSize.x}>
-      <ContextMenu
-        iconSize={iconSize}
-        iconLayout={iconLayout}
-        sortType={sortType}
-        setIconSize={setIconSize}
-        setSortType={setSortType}
-        setIconLayout={setIconLayout}
-      >
-        <SimpleGrid
-          templateColumns={`repeat(${iconLayout.x}, 1fr)`}
-          templateRows={`repeat(${iconLayout.y}, 1fr)`}
-          h={"100%"}
-          w={"100%"}
-          gap={1}
+    <>
+      <GridItem rowStart={1} rowSpan={screenSize.y - 1} colSpan={screenSize.x}>
+        <ContextMenu
+          iconSize={iconSize}
+          iconLayout={iconLayout}
+          sortType={sortType}
+          setIconSize={setIconSize}
+          setSortType={setSortType}
+          setIconLayout={setIconLayout}
         >
-          {Object.entries(exports).map(([key]) => (
-            <GridItem
-              rowSpan={2}
-              colSpan={2}
-              key={key}
-              border={"1px solid white"}
-            >
-              <Dialog.Root>
-                <Dialog.Trigger asChild>
-                  <Button alignItems={"center"} justifyContent={"center"}>
-                    <Stack>
-                      <AvatarGroup>
-                        <Avatar.Root>
-                          <Avatar.Fallback />
-                          <Avatar.Image />
-                        </Avatar.Root>
-                      </AvatarGroup>
-                      test
-                    </Stack>
-                  </Button>
-                </Dialog.Trigger>
-                <Portal>
-                  <Dialog.Backdrop />
-                  <Dialog.Positioner>
-                    <Dialog.Content
-                      w={"50vw"}
-                      h={"100vh"}
-                      p={0}
-                      position={"fixed"}
-                      top={0}
-                      left={0}
-                      m={0}
-                    >
-                      <Dialog.Body asChild p={0}>
-                        <SimpleGrid
-                          templateColumns={"repeat(20, 1fr)"} // 4 columns
-                          templateRows={"repeat(20, 1fr)"} // 4 rows
-                        >
-                          <GridItem colSpan={20} rowSpan={1} asChild>
-                            <Stack
-                              direction={"row"}
-                              justifyContent={"space-between"}
-                            >
-                              Naruto Form
-                              <AbsoluteCenter position={"relative"}>
-                                <Dialog.CloseTrigger
-                                  asChild
-                                  alignItems={"center"}
-                                  justifyContent={"center"}
-                                >
-                                  <CloseButton bg="red" />
-                                </Dialog.CloseTrigger>
-                              </AbsoluteCenter>
-                            </Stack>
-                          </GridItem>
-                        </SimpleGrid>
-                      </Dialog.Body>
-                    </Dialog.Content>
-                  </Dialog.Positioner>
-                </Portal>
-              </Dialog.Root>
-            </GridItem>
-          ))}
-        </SimpleGrid>
-      </ContextMenu>
-    </GridItem>
+          <SimpleGrid
+            templateColumns={`repeat(${iconLayout.x}, 1fr)`}
+            templateRows={`repeat(${iconLayout.y}, 1fr)`}
+            h={"100%"}
+            w={"100%"}
+            gap={1}
+          >
+            {Object.entries(exports).map(([key]) => (
+              <GridItem
+                rowSpan={2}
+                colSpan={2}
+                key={key}
+                border={"1px solid white"}
+              >
+                <Dialog.Root>
+                  <Dialog.Trigger asChild>
+                    <Button alignItems={"center"} justifyContent={"center"}>
+                      <Stack>
+                        <AvatarGroup>
+                          <Avatar.Root>
+                            <Avatar.Fallback />
+                            <Avatar.Image />
+                          </Avatar.Root>
+                        </AvatarGroup>
+                        test
+                      </Stack>
+                    </Button>
+                  </Dialog.Trigger>
+                  <Portal>
+                    <Dialog.Backdrop />
+                    <Dialog.Positioner>
+                      <Dialog.Content
+                        w={"50vw"}
+                        h={"100vh"}
+                        p={0}
+                        position={"fixed"}
+                        top={0}
+                        left={0}
+                        m={0}
+                      >
+                        <Dialog.Body asChild p={0}>
+                          <SimpleGrid
+                            templateColumns={"repeat(20, 1fr)"} // 4 columns
+                            templateRows={"repeat(20, 1fr)"} // 4 rows
+                          >
+                            <GridItem colSpan={20} rowSpan={1} asChild>
+                              <Stack
+                                direction={"row"}
+                                justifyContent={"space-between"}
+                              >
+                                Naruto Form
+                                <AbsoluteCenter position={"relative"}>
+                                  <Dialog.CloseTrigger
+                                    asChild
+                                    alignItems={"center"}
+                                    justifyContent={"center"}
+                                  >
+                                    <CloseButton bg="red" />
+                                  </Dialog.CloseTrigger>
+                                </AbsoluteCenter>
+                              </Stack>
+                            </GridItem>
+                          </SimpleGrid>
+                        </Dialog.Body>
+                      </Dialog.Content>
+                    </Dialog.Positioner>
+                  </Portal>
+                </Dialog.Root>
+              </GridItem>
+            ))}
+          </SimpleGrid>
+        </ContextMenu>
+      </GridItem>
+    </>
   );
 }
